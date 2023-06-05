ac0df85083e7b6f206c531f4b3c2787cd9efca72
Talo2520 <90891244+Talo2520@users.noreply.github.com>
2023-06-05 13:26:11 +0200

    Add files via upload

---
+++ Scripts/SIR.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SIR : MonoBehaviour
+{
+    private Text text;
+    private PandemicArea pandemicArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pandemicArea = GetComponentInParent<PandemicArea>();
+        text = GetComponent<Text>();      
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        text.text = "S = " + pandemicArea.healthyCounter +
+                    "  I = " + pandemicArea.infectedCounter  +
+                    "  R = " + pandemicArea.recoveredCounter + "\n" + 
+                    "Time = " + Mathf.FloorToInt(Time.time);
+        
+    }
+}
